fe0e5ffe4cca0406db88a2af97369cfa62171b94
Vincent Sposato <vsposato@enhancedmedicationservices.com>
2023-02-11 16:33:45 -0500

    Updated items

--- generate_diagrams.sh
+++ generate_diagrams.sh
@@ -1,9 +1,30 @@
 #!/usr/bin/env bash
 
 export PLANT_UML='addons/plantuml-1.2023.1.jar'
-export FILES=$(find . -path ./addons -prune -o -name "*.puml" -print)
+export PLANT_UML_FILES=$(find . -path ./addons -prune -o -name "*.puml" -print)
+export PLANT_UML_PNG_FILES=$(find . -path ./addons -prune -o -name "*.png" -print)
 
-for f in $FILES
+
+for f in $PLANT_UML_PNG_FILES
+do
+  rm $f
+done
+
+for f in $PLANT_UML_FILES
 do
   java -jar $PLANT_UML $f -tpng
+done
+
+export ASCII_DOC_SHELL='addons/asciidoctorj-2.5.4/bin/asciidoctorj'
+export ASCII_DOC_FILES=$(find . -path ./addons -prune -o -name "*.adoc" -print)
+export ASCII_DOC_HTML_FILES=$(find . -path ./addons -prune -o -name "*.html" -print)
+
+for f in $ASCII_DOC_HTML_FILES
+do
+  rm $f
+done
+
+for f in $ASCII_DOC_FILES
+do
+  $ASCII_DOC_SHELL $f
 done
